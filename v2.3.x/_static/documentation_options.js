99f518f249203531c1782aac0c7a5598bbb49c74
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-10-03 14:46:53 +0000

    Update docs after building Travis build 9339 of SciTools/iris
    The docs were built from the branch 'v2.3.x' against the commit
    aa9f8fe0677d583df078528fec9a83965ece8252.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/SciTools/iris/jobs/593080262.
    
    The doctr command that was run is
    
        /home/travis/miniconda/envs/test-environment/bin/doctr deploy --deploy-repo SciTools-docs/iris --built-docs docs/iris/build/html --key-path .github/deploy_key.scitools-docs.enc --no-require-master v2.3.x

--- v2.3.x/_static/documentation_options.js
+++ v2.3.x/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.3.0dev0',
+    VERSION: '2.3.0rc0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
